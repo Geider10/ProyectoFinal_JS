b20c9d3555227cbe3e80a4c5dbf3a3f8466bf966
Geider10 <geiderde20@gmail.com>
2023-12-24 15:57:44 -0300

    agregue funciones y condicional

--- logica.js
+++ logica.js
@@ -1,17 +1,38 @@
-let capital = parseFloat(prompt("Enter capital"));
-let coutas = parseFloat(prompt("Enter coutas"));
-let interes = parseFloat(prompt("Interes:"));
+let capital = 0, cuotas = 0, interes = 0;
+let coutaMes = 0, pagoMes = 0,interesMes = 0, coutaMasInteres = 0;
+let interesTotal = 0, pagoTotal = 0;
 
-let coutaMes= capital / coutas;
-let pagoMes= coutaMes.toFixed(2);
-let interesMes = (capital * interes) / 100;
-let coutaMasInteres = parseInt(pagoMes) + interesMes;
-
-let interesTotal = interesMes * coutas;
-let resumenTotal = capital + interesTotal;
-
-for(let i = 0; i <= coutas; i++){
-    console.log("coutas " + i + " PagoXMes "+ pagoMes + " InteresXMes "+ interesMes + " CoutaFinal "  + coutaMasInteres);
-    // console.log("<br>");
+let GetInput = () =>{
+    capital = parseFloat(prompt("Ingresa el monto a solicitar: "));
+    cuotas = parseFloat(prompt("Ingresa las cuotas a pagar: "));
+    interes = parseFloat(prompt("Interes es %: "));
 }
-console.log("Factura Total " + resumenTotal);
+let Operations = ()=>{
+    coutaMes= capital / cuotas;
+    pagoMes= coutaMes.toFixed(2);
+    interesMes = (capital * interes) / 100;
+    coutaMasInteres = parseInt(pagoMes) + interesMes;
+    //data of output
+    interesTotal = interesMes * cuotas;
+    pagoTotal = capital + interesTotal;
+}
+let SetOutput= ()=>{
+    for(let i = 1; i <= cuotas; i++){
+        console.log("cuotas " + i + " PagoXMes "+ pagoMes + " InteresXMes "+ interesMes );
+    }
+    console.log("Pago por mes: "+coutaMasInteres);
+    console.log("Total a pagar: "+pagoTotal);
+}
+let confirmar;
+let System = ()=>{
+    confirmar=confirm("Estas seguro de pedir un prestamo?");
+    if(confirmar == true){
+        GetInput();
+        Operations();
+        SetOutput();
+    }
+    else{
+        alert("Decidiste no hacer el prestamo");
+    }
+}
+System();
