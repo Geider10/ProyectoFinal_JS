932a680e903db81c0b9fb3ac75e34f6f29d7933b
Geider10 <geiderde20@gmail.com>
2024-01-07 11:10:50 -0300

    le puse validacion regex a los inputs y a los botones le agregue su evento

--- ui.js
+++ ui.js
@@ -1,47 +1,110 @@
-let formulario = document.querySelector(".formu");
-let btnCal = document.getElementById("btnCalculate");
-let btnRes = document.getElementById("btnReset");
-// console.log(btnCal.hasAttribute("Id"));
-// console.log(btnCal.hasAttribute("class"))
-// console.log(btnCal)
-// btnCal.setAttribute("class","boton");
-// btnCal.removeAttribute("class");
-
-let montoSolicitar, coutas, interes;
-
-let getInput= ()=>{
- 
-    montoSolicitar = formulario.children[1].getAttribute("value");
-    coutas = formulario.children[3].getAttribute("value");
-    interes = formulario.children[5].getAttribute("value");
-    console.log(formulario.children[1].getAttribute("value"));
-    console.log(formulario.children[3].getAttribute("value"));
-    console.log(formulario.children[5].getAttribute("value"));
-    console.log(btnCal.hasAttribute("id"));
+//validar lista de inputs
+const inputs = document.querySelectorAll(".formu .user");
+const readingnputs = document.querySelectorAll(".formu .back");
+const expresion = /^\d+$/;
+const estadoInput = {
+    capital : false,
+    cuotas: false,
+    interes: false
 }
+//convertir inputs de tipo lectura
+readingnputs.forEach((r)=>{
+    r.setAttribute("readonly","true")
+})
+const validarCampo = (input,campo)=>{
+    if(expresion.test(input.value)){
+        document.getElementById(campo).classList.remove("borderMal");
+        document.getElementById(campo).classList.add("borderBien");
+        document.querySelector(`#grupo${campo} p`).classList.add("textOff");
+        estadoInput[campo] = true;
+    }
+    else{
+        document.getElementById(campo).classList.remove("borderBien");
+        document.getElementById(campo).classList.add("borderMal");
+        document.querySelector(`#grupo${campo} p`).classList.remove("textOff");
+        estadoInput[campo] = false;
+    }
+}
+const validarFormulario = (e)=>{
+    switch(e.target.name){
+        case "capital":
+            validarCampo(e.target,"capital");
+        break;
+        case "cuotas":
+            validarCampo(e.target,"cuotas");      
+        break;
+        case "interes":
+            validarCampo(e.target,"interes");      
+        break;
+    }
+}
+inputs.forEach((input)=>{
+    input.addEventListener("keyup",validarFormulario);
+    input.addEventListener("blur",validarFormulario);
+})
+//operations
+const formulario = document.querySelector(".formu");
+const btnCal = document.getElementById("btnCalculate");
+const btnRes = document.getElementById("btnReset");
 
+let capital,cuotas,interes;
+const getInput=()=>{
+    capital = parseInt(document.getElementById("capital").value);
+    cuotas = parseInt(document.getElementById("cuotas").value);
+    interes = parseInt(document.getElementById("interes").value);
+}
 let cuotasMes = 0, interesMes = 0, pagoMes = 0, pagoTotal =0 ;
 let operation= () =>{
-    cuotasMes = parseFloat(montoSolicitar) / parseInt(coutas);
-    interesMes = (parseFloat(montoSolicitar) * parseFloat(interes)) / 100;
-    pagoMes = parseInt(cuotasMes) + parseInt(interesMes);
-    pagoTotal = (parseInt(interesMes) * parseInt(coutas)) + parseFloat(montoSolicitar);
+    cuotasMes = capital / cuotas;
+    interesMes = (capital * interes)/100;
+    pagoMes = cuotasMes+ interesMes;
+    pagoTotal = (interesMes * cuotas) + capital;
+    console.log("La couta al mes es: "+cuotasMes);
+    console.log("El interes al mes es: "+interesMes);
+    console.log("El interes + cuotas a pagar es: "+pagoMes);
+    console.log("El pago total es: "+pagoTotal);
 }
-// operation();
+// setear data
 let inputMes, inputTotal;
-let setOutput = (m,p) =>{
-    inputMes = formulario.children[7].setAttribute("value", m);
-    inputTotal = formulario.children[9].setAttribute("value", p);
+let setOutput = () =>{
+    inputMes = document.getElementById("pagoMes");
+    inputTotal = document.getElementById("pagoTotal");
+    inputMes.value = pagoMes.toFixed(2);
+    inputTotal.value = pagoTotal;
 }
-function clean(){
-    montoSolicitar.setAttribute("value", "");
-    coutas.removetAttribute("value","");
-    interes.setAttribute("value","");
-    inputMes.setAttribute("value","");
-    inputTotal.setAttribute("value","");
-}
-
-btnCal.addEventListener("click",() =>{
+let diagramaFlujo = ()=>{
     getInput();
+    operation();
+    setOutput();
+}
+btnCal.addEventListener("click",()=>{
+    if(estadoInput.capital && estadoInput.cuotas && estadoInput.interes){
+        document.getElementById("forBien").classList.remove("textOff");
+        setTimeout(()=>{
+            document.getElementById("forBien").classList.add("textOff");
+        },5000)
+        diagramaFlujo();
+    }
+    else{
+        document.getElementById("forMal").classList.remove("textOff"); 
+        setTimeout(() => {
+            document.getElementById("forMal").classList.add("textOff");
+        }, 5000);
+    }
 });
-btnRes.addEventListener("click",clean)
+btnRes.addEventListener("click", (e)=>{
+    inputs.forEach((i)=>{
+        i.classList.remove("borderBien");
+        i.classList.remove("borderMal");
+        let textError=document.querySelectorAll(".formu p");
+        textError.forEach((e)=>{
+            e.classList.add("textOff")
+        })
+    })
+    estadoInput.capital = false;
+    estadoInput.cuotas = false;
+    estadoInput.cuotas = false;
+    
+    e.preventDefault();
+    formulario.reset();
+});
